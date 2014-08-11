682626ebf4a8075550c33e6c8f59b05d846ffafa
Charles Worthignton <c.e.worthington@gmail.com>
2014-08-11 03:05:25 -0400

    smooth scrolling

--- assets/js/site.js
+++ assets/js/site.js
@@ -17,7 +17,7 @@ $(function(){
   });
 });
 
-jQuery(function($) {
+$(function($) {
   $(document).ready( function() {
     $('#navigation_bar').stickUp({
                   parts: {
@@ -38,5 +38,20 @@ jQuery(function($) {
                   itemClass: 'play_square',
                   itemHover: 'active_play'
                 });
+  });
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
   });
 });
