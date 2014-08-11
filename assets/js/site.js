0c69573e5d787b0153ce0a65fb496d18334bee53
Charles Worthignton <c.e.worthington@gmail.com>
2014-08-11 02:54:20 -0400

    add navigation bar highlight effect

--- assets/js/site.js
+++ assets/js/site.js
@@ -17,26 +17,26 @@ $(function(){
   });
 });
 
-// jQuery(function($) {
-//   $(document).ready( function() {
-//     $('#introduction').stickUp({
-//                   // parts: {
-//                   //   0: 'play1',
-//                   //   1: 'play2',
-//                   //   2: 'play3',
-//                   //   3: 'play4',
-//                   //   4: 'play5',
-//                   //   5: 'play6',
-//                   //   6: 'play7',
-//                   //   7: 'play8',
-//                   //   8: 'play9',
-//                   //   9: 'play10',
-//                   //   10: 'play11',
-//                   //   11: 'play12',
-//                   //   12: 'play13'
-//                   // },
-//                   // itemClass: 'play_square',
-//                   // itemHover: 'active_play'
-//                 });
-//   });
-// });
+jQuery(function($) {
+  $(document).ready( function() {
+    $('#navigation_bar').stickUp({
+                  parts: {
+                    0: 'play1',
+                    1: 'play2',
+                    2: 'play3',
+                    3: 'play4',
+                    4: 'play5',
+                    5: 'play6',
+                    6: 'play7',
+                    7: 'play8',
+                    8: 'play9',
+                    9: 'play10',
+                    10: 'play11',
+                    11: 'play12',
+                    12: 'play13'
+                  },
+                  itemClass: 'play_square',
+                  itemHover: 'active_play'
+                });
+  });
+});
